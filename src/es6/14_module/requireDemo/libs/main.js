aa680b7760f5cab49ec3c9707f37fb8a4313beb8
MessiHL <roadsclub@163.com>
2022-10-11 09:41:14 +0800

    docs:注释完善

--- src/es6/14_module/requireDemo/libs/main.js
+++ src/es6/14_module/requireDemo/libs/main.js
@@ -3,8 +3,16 @@
  * @Autor: hl
  * @Date: 2022-10-11 09:14:25
  * @LastEditors: hl
- * @LastEditTime: 2022-10-11 09:34:16
+ * @LastEditTime: 2022-10-11 09:40:24
  */
+
+/*
+    1- require 的第一个参数表示依赖的模块的路径，第二个参数表示此模块的内容。
+    2- requirejs 为全局添加了 define 函数，你只要按照这种约定的方式书写这个模块即可。
+    3- require.js 会自动分析依赖关系，将需要加载的模块正确加载。
+    
+    此处，主模块（main.js）依赖 add 模块和 square 模块
+*/
 require(['./add','./square'],function(addModule,squareModule){
     console.log(addModule.add(1,2));
     console.log(squareModule.square(8));
--- src/es6/14_module/requireDemo/libs/square.js
+++ src/es6/14_module/requireDemo/libs/square.js
@@ -1,10 +1,12 @@
 /*
- * @Description: 平方，依赖于./multiply.js
+ * @Description: 平方。依赖于./multiply.js
  * @Autor: hl
  * @Date: 2022-10-11 09:14:41
  * @LastEditors: hl
- * @LastEditTime: 2022-10-11 09:35:06
+ * @LastEditTime: 2022-10-11 09:39:06
  */
+
+
 define(['./multiply'], function(multiplyModule) {
     console.log('加载了 square 模块');
     return{
